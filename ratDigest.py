d04096a91e5a8b51e88e11d6e6b12b401fe65a01
David Pinney <david@pinney.org>
2017-09-23 10:14:17 -0400

    New source file for new approach

---
+++ ratDigest.py
@@ -0,0 +1,13 @@
+''' Configures a GridLAB-D model, runs it, then modifies the output to create things that look like sensor readings from it.'''
+
+INPUT_EXAMPLE = {
+	'gldFilePath':'TBD',
+	'probabilityThing':0.97
+}
+
+def configureAndRunGld():
+	pass
+
+def modifyOutput():
+	pass
+
