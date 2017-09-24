1695cd3fd20cf1123bbbfbc601c806bcb95aa582
David Pinney <david@pinney.org>
2017-09-24 15:46:47 -0400

    Choosing small test case, workflow.

--- ratDigest.py
+++ ratDigest.py
@@ -1,13 +1,34 @@
-''' Configures a GridLAB-D model, runs it, then modifies the output to create things that look like sensor readings from it.'''
+'''RatDigest Workflow'''
 
-INPUT_EXAMPLE = {
-	'gldFilePath':'TBD',
-	'probabilityThing':0.97
+input = {
+	'pathToGlm':'./littleTest/smsSingle.glm',
+	'preProc': {
+		'startTime':'2017-01-01 00:00',
+		'endTime':'2017-01-02 00:00',
+		'timeStep':4
+	},
+	'postProc': {
+		'voltAlarmMin':110,
+		'voltAlarmMax':130,
+		'powerAlarmMin':0,
+		'powerAlarmMax':9000,
+		'probMessageSend':0.99
+	}
 }
 
-def configureAndRunGld():
+CONSTANTS = {
+	'meterReadInterval':15*60
+}
+
+output = [
+	{'deviceId':'blah','metricA':34}
+]
+
+def go(input):
+	#preProc: modify glm file
+	#runGld: just run it
+	#postProc: take csvs, make nice output
 	pass
 
-def modifyOutput():
-	pass
-
+if __name__ == '__main__':
+	print 'hay'
