43b7a7d91fc0204f9ced89bee54e20f8abcc39e2
David Pinney <david@pinney.org>
2017-09-26 08:27:31 -0400

    Switching, regulator and meter disconnect injection.

--- ratDigest.py
+++ ratDigest.py
@@ -14,7 +14,12 @@ exampleInput = {
 		'meterNames':['tm_1','tm_2'],
 		'regulatorNames':['Reg1'],
 		'regulatorReadInterval':'4',
-		'switchNames':['newSwitch']
+		'switchNames':['newSwitch'],
+		'scheduledEvents':[
+			{'parent':'tm_1','property':'service_status','schedule':'2017-01-01 12:00,1;2017-01-01 12:15,1;2017-01-01 12:30,0;2017-01-01 12:45,0'},
+			{'parent':'newSwitch','property':'status','schedule':'2017-01-01 12:00,1;2017-01-01 12:15,1;2017-01-01 12:30,0;2017-01-01 12:45,0'},
+			{'parent':'Reg1','property':'tap_A','schedule':'2017-01-01 12:00,2;2017-01-01 12:15,1;2017-01-01 12:30,1;2017-01-01 12:45,2'}
+		],
 	},
 	'postProc': {
 		'voltAlarmMin':110,
@@ -30,6 +35,13 @@ def go(inDict):
 	# PREPROCESS to create and run a new .glm file. #
 	#################################################
 	FILE_UID = 'ratDigest'
+	# Remove old outputs.
+	for fName in os.listdir(inDict['glmDirPath']):
+		try:
+			if fName.startswith(FILE_UID):
+				os.remove(inDict['glmDirPath'] + fName)
+		except:
+			pass # tried but couldn't delete old output.
 	# Templates used to rewrite .glm:
 	timeTemplate = 'clock {\n' +\
 		'\ttimezone PST+8PDT;\n' +\
@@ -43,6 +55,12 @@ def go(inDict):
 		'\tparent INSERT_PARENT;\n' +\
 		'\tproperty INSERT_PROPERTIES;\n' +\
 		'};\n\n'
+	playerTemplate = 'object player {\n' +\
+		'\tfile INSERT_FILE_NAME.player;\n' +\
+		'\tparent INSERT_PARENT;\n' +\
+		'\tproperty INSERT_PROPERTIES;\n' +\
+		'};\n\n'
+	allPlayers = 'class player {double value;};\n\n'
 	allRecorders = ''
 	# Modify templates to reflect input parameters.
 	timeTemplate = timeTemplate.replace('INSERT_START_TIME',inDict['preProc']['startTime'])
@@ -54,31 +72,34 @@ def go(inDict):
 		tempTemplate = tempTemplate.replace('INSERT_INTERVAL', inDict['preProc']['meterReadInterval'])
 		tempTemplate = tempTemplate.replace('INSERT_PARENT', meterName)
 		tempTemplate = tempTemplate.replace('INSERT_PROPERTIES', 'measured_real_energy, measured_real_power, measured_reactive_power, measured_voltage_1, measured_voltage_2')
-		allRecorders += (tempTemplate)
+		allRecorders += tempTemplate
 	for regName in inDict['preProc']['regulatorNames']:
 		tempTemplate = str(recorderTemplate)
 		tempTemplate = tempTemplate.replace('INSERT_FILE_NAME', FILE_UID + '_REG_' + regName)
 		tempTemplate = tempTemplate.replace('INSERT_INTERVAL', inDict['preProc']['regulatorReadInterval'])
 		tempTemplate = tempTemplate.replace('INSERT_PARENT', regName)
 		tempTemplate = tempTemplate.replace('INSERT_PROPERTIES', 'power_out_A, power_out_B, power_out_C, current_out_A, current_out_B, current_out_C, tap_A, tap_B, tap_C')
-		allRecorders += (tempTemplate)
+		allRecorders += tempTemplate
 	for switchName in inDict['preProc']['switchNames']:
 		tempTemplate = str(recorderTemplate)
 		tempTemplate = tempTemplate.replace('INSERT_FILE_NAME', FILE_UID + '_SWITCH_' + switchName)
 		tempTemplate = tempTemplate.replace('INSERT_INTERVAL', inDict['preProc']['meterReadInterval']) #TODO: switch interval.
 		tempTemplate = tempTemplate.replace('INSERT_PARENT', switchName)
 		tempTemplate = tempTemplate.replace('INSERT_PROPERTIES', 'phase_A_state, phase_B_state, phase_C_state')
-		allRecorders += (tempTemplate)
-	# Remove old outputs.
-	for fName in os.listdir(inDict['glmDirPath']):
-		try:
-			if fName.startswith(FILE_UID):
-				os.remove(inDict['glmDirPath'] + fName)
-		except:
-			pass # tried but couldn't delete old output.
+		allRecorders += tempTemplate
+	for event in inDict['preProc']['scheduledEvents']:
+		tempTemplate = str(playerTemplate)
+		fileName = FILE_UID + '_' + event['parent'] + '_' + event['property']
+		tempTemplate = tempTemplate.replace('INSERT_FILE_NAME', fileName)
+		tempTemplate = tempTemplate.replace('INSERT_PARENT', event['parent'])
+		tempTemplate = tempTemplate.replace('INSERT_PROPERTIES', event['property'])
+		allPlayers += tempTemplate
+		with open(inDict['glmDirPath'] + fileName + '.player', 'w') as pFile:
+			playerContent = event['schedule'].replace(';','\n')
+			pFile.write(playerContent)
 	# Modify and write the new GLM.
 	glmContent = open(inDict['glmDirPath'] + inDict['glmName'],'r').read()
-	glmContent = timeTemplate + glmContent + allRecorders
+	glmContent = timeTemplate + glmContent + allPlayers + allRecorders
 	with open(inDict['glmDirPath'] + FILE_UID + '.glm', 'w') as outFile:
 		outFile.write(glmContent)
 	# Run the new GLM.
