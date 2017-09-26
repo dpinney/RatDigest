ebdf1aa4562d4cecb86e8d26641082ffd3cc1f3b
David Pinney <david@pinney.org>
2017-09-26 12:54:10 -0400

    Added attacks.

--- ratDigest.py
+++ ratDigest.py
@@ -1,15 +1,15 @@
-'''RatDigest Workflow'''
+'''RatDigest Makes Network Messages From GridLAB-D Simulations.'''
 
-import os, subprocess, csv, json
-from pprint import pprint
+import os, subprocess, csv, json, random
+from datetime import datetime, timedelta
 
-exampleInput = {
+testInput = {
 	'glmDirPath':'./littleTest/',
 	'glmName':'smsSingle.glm',
 	'preProc': {
 		'startTime':'2017-01-01 12:00:00',
 		'stopTime':'2017-01-01 13:00:00',
-		'timeStep':'4',
+		'simTimeStep':'4',
 		'meterReadInterval':'900',
 		'meterNames':['tm_1','tm_2'],
 		'meterLoadMap':{'house1':'tm_1','house2':'tm_2'},
@@ -17,23 +17,22 @@ exampleInput = {
 		'regulatorReadInterval':'450',
 		'switchNames':['newSwitch'],
 		'controlActions':[
-			{'identifier':'MS-IniateDisconnectConnect','parent':'tm_1','property':'service_status','schedule':'2017-01-01 12:30,0'},
-			{'identifier':'DNP-SubstationBreakerSwitchStatus','parent':'newSwitch','property':'status','schedule':'2017-01-01 12:30,0'},
-			{'identifier':'DNP-SubstationVoltageControl','parent':'Reg1','property':'tap_A','schedule':'2017-01-01 12:00,2;2017-01-01 12:15,1;2017-01-01 12:45,2'},
-			{'identifier':'INTENTIONAL_FAULT','parent':'bigload','property':'base_power','schedule':'2017-01-01 12:45,60.0 kW'}
+			{'identifier':'MS-IniateDisconnectConnect','parent':'tm_1','property':'service_status','schedule':'2017-01-01 12:30:00 PST,0'},
+			{'identifier':'DNP-SubstationBreakerSwitchWrite','parent':'newSwitch','property':'status','schedule':'2017-01-01 12:30:00 PST,0'},
+			{'identifier':'DNP-SubstationVoltageControl','parent':'Reg1','property':'tap_A','schedule':'2017-01-01 12:00:00 PST,2;2017-01-01 12:15:00 PST,1;2017-01-01 12:45:00 PST,2'},
+			{'identifier':'INTENTIONAL_FAULT','parent':'bigload','property':'base_power','schedule':'2017-01-01 12:45:00 PST,60.0 kW'}
 		],
 	},
 	'postProc': {
-		'powerAlarmMin':0,
-		'powerAlarmMax':9000,
-		'probMessageSend':0.99
+		'dosList':[{'device':'Reg1','start':'2017-01-01 12:07:00 PST','end':'2017-01-01 12:16:00 PST'}],
+		'floodList':[{'device':'tm_2','type':'alarm','quantity':5,'start':'2017-01-01 12:20:00 PST','end':'2017-01-01 12:40:00 PST'}],
 	}
 }
 
-def go(inDict):
-	#################################################
-	# PREPROCESS to create and run a new .glm file. #
-	#################################################
+def pre(inDict):
+	#############################
+	# Generate a new .glm file. #
+	#############################
 	FILE_UID = 'ratDigest'
 	os.chdir(inDict['glmDirPath'])
 	# Remove old outputs.
@@ -66,7 +65,7 @@ def go(inDict):
 	# Modify templates to reflect input parameters.
 	timeTemplate = timeTemplate.replace('INSERT_START_TIME',inDict['preProc']['startTime'])
 	timeTemplate = timeTemplate.replace('INSERT_STOP_TIME',inDict['preProc']['stopTime'])
-	timeTemplate = timeTemplate.replace('INSERT_TIME_STEP',inDict['preProc']['timeStep'])
+	timeTemplate = timeTemplate.replace('INSERT_TIME_STEP',inDict['preProc']['simTimeStep'])
 	for meterName in inDict['preProc']['meterNames']:
 		tempTemplate = str(recorderTemplate)
 		tempTemplate = tempTemplate.replace('INSERT_FILE_NAME', FILE_UID + '_METER_' + meterName)
@@ -84,7 +83,7 @@ def go(inDict):
 	for switchName in inDict['preProc']['switchNames']:
 		tempTemplate = str(recorderTemplate)
 		tempTemplate = tempTemplate.replace('INSERT_FILE_NAME', FILE_UID + '_SWITCH_' + switchName)
-		tempTemplate = tempTemplate.replace('INSERT_INTERVAL', inDict['preProc']['meterReadInterval']) #TODO: switch interval.
+		tempTemplate = tempTemplate.replace('INSERT_INTERVAL', inDict['preProc']['regulatorReadInterval'])
 		tempTemplate = tempTemplate.replace('INSERT_PARENT', switchName)
 		tempTemplate = tempTemplate.replace('INSERT_PROPERTIES', 'phase_A_state, phase_B_state, phase_C_state')
 		allRecorders += tempTemplate
@@ -104,12 +103,12 @@ def go(inDict):
 	with open(FILE_UID + '.glm', 'w') as outFile:
 		outFile.write(glmContent)
 	# Run the new GLM.
-	with open(FILE_UID + '_stdout.txt','w') as stdout, open(FILE_UID + '_stderr.txt','w') as stderr:
+	with open(FILE_UID + '_xstdout.txt','w') as stdout, open(FILE_UID + '_xstderr.txt','w') as stderr:
 		proc = subprocess.Popen(['gridlabd','ratDigest.glm'], stdout=stdout, stderr=stderr)
 		returnCode = proc.wait()
-	#########################################################
-	# POSTPROCESS to turn the CSV data in to a message list #
-	#########################################################
+	#######################################################
+	# Turn GridLAB-D output CSV data in to a message list #
+	#######################################################
 	allFileNames = os.listdir('.')
 	csvFileNames = [x for x in allFileNames if x.startswith(FILE_UID) and x.endswith('.csv')]
 	output = []
@@ -130,6 +129,8 @@ def go(inDict):
 					row['identifier'] = 'MS-GetLatestReadings'
 				elif fName.startswith(FILE_UID + '_REG_'):
 					row['identifier'] = 'DNP-SubstationKwH'
+				elif fName.startswith(FILE_UID + '_SWITCH_'):
+					row['identifier'] = 'DNP-SubstationBreakerSwitchStatus'
 				else:
 					row['identifier'] = 'unknown'
 				output.append(row)
@@ -146,21 +147,70 @@ def go(inDict):
 			}
 			output.append(outMessage)
 	# Add alarms to output.
-	with open(FILE_UID + '_stderr.txt') as errFile:
+	with open(FILE_UID + '_xstderr.txt') as errFile:
 		lines = errFile.read().split('\n')
 		for x in lines:
 			if "is outside of ANSI standards" in x:
 				out = {}
 				out['identifier'] = 'MS-ODEventNotification'
 				out['type'] = 'voltage alarm'
-				out['location'] = x[x.find(' : ') + 3:x.find(' - ')]
+				rawLocation = x[x.find(' : ') + 3:x.find(' - ')]
+				out['location'] = inDict['preProc']['meterLoadMap'][rawLocation]
 				magnitude = x[x.find(' = ') + 3 :x.find(' percent')]
 				out['magnitude'] = str(float(magnitude)/100.0 * 120.0)
 				out['timestamp'] = x[10:33]
 				output.append(out)
 	# Write full output.
-	with open(FILE_UID + '.json','w') as outFile:
+	with open(FILE_UID + 'PreAttack.json','w') as outFile:
 		json.dump(output, outFile, indent=4)
+	# Also return output for post processing.
+	return output
+
+def post(inDict, messages):
+	# Helper function for timestamp parsing.
+	def tParse(timeString):
+		return datetime.strptime(timeString[0:-4], '%Y-%m-%d %H:%M:%S')
+	# Helper function for random dates in a range.
+	def randomDate(start, end):
+		delta = end - start
+		int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+		random_second = random.randrange(int_delta)
+		return start + timedelta(seconds=random_second)
+	# Perform DOS attacks by dropping messages.
+	for attack in inDict['postProc']['dosList']:
+		start = tParse(attack['start'])
+		end = tParse(attack['end'])
+		for message in messages:
+			time = tParse(message['timestamp'])
+			if start < time < end:
+				del message
+	# Perform flood attacks.
+	for attack in inDict['postProc']['floodList']:
+		start = tParse(attack['start'])
+		end = tParse(attack['end'])
+		if attack['type'] is 'alarm':
+			underVoltage = random.randrange(95.0,110.0)
+			overVoltage = random.randrange(130.0,145.0)
+			magnitude = random.choice([underVoltage,overVoltage])
+			messageTemplate = {
+				'identifier': 'MS-ODEventNotification', 
+				'type': 'voltage alarm', 
+				'location': attack['device'], 
+				'magnitude': str(magnitude),
+				'fake':'True'
+			}
+		else:
+			raise Exception('No type specified for attack ' + str(attack))
+		for count in xrange(attack['quantity']):
+			fakeMessage = dict(messageTemplate)
+			fakeMessage['timestamp'] = datetime.strftime(randomDate(start, end), '%Y-%m-%d %H:%M:%S PST')
+			messages.append(fakeMessage)
+	# Dump results.
+	with open('ratDigestPostAttack.json','w') as outFile:
+		json.dump(messages, outFile, indent = 4)
+	# And return as well.
+	return messages
 
 if __name__ == '__main__':
-	go(exampleInput)
+	out = pre(testInput)
+	attackedOut = post(testInput, out)
